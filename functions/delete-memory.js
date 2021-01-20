99690712c3bec6b96252eb1df9beedd0a2182878
Miranda Riley <mirandariley@Mirandas-MacBook-Air-2.local>
2021-01-19 22:45:35 -0600

    delete entries

--- functions/delete-memory.js
+++ functions/delete-memory.js
@@ -2,18 +2,22 @@ const faunadb = require('faunadb')
 const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 const q = faunadb.query
 
-const handler = async (event) => {
-  if (event.httpMethod !== 'GET'){
-    return { statusCode: 500, body: 'GET OUTTA HERE!' }
-  }
+exports.handler = async (event, context) => {
 
-  try {
-    const memory = JSON.parse(event.body)
-const memId = memory.ref['@ref'].id
-const req = await faunaClient.query(q.Delete(q.Ref(`classes/memories/${memId}`)))
-  } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
+    if (event.httpMethod !== 'DELETE'){
+      return { statusCode: 500, body: "This is a place of destruction. Not what you wanted..." }
+    }
+    
+    try {
+      const memory = JSON.parse(event.body)
+      const memId = memory.ref['@ref'].id
+      const req = await faunaClient.query(q.Delete(q.Ref(`classes/memories/${memId}`)))
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ mem: req })
+      }
+    } catch (err) {
+      return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
+    }
   }
-}
-
-module.exports = { handler }
